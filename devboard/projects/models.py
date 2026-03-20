6e41d9aa726e70903145953955a15aa183137ca4
Bharat Panchal <bharatpanchal3578@gmail.com>
2026-03-20 12:24:15 +0530

    feat(projects): add Project model with relationships and admin registration

--- devboard/projects/models.py
+++ devboard/projects/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owned_projects") # user.owned_projects.all() → projects user owns
+    members = models.ManyToManyField(User, related_name="projects") # user.projects.all() → projects user is member of
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
