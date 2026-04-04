b6901ff7b05c4820ca00e96e878d9612e53ea4ed
Bharat Panchal <bharatpanchal3578@gmail.com>
2026-04-04 20:36:13 +0530

    register Comment model in admin

--- devboard/tasks/admin.py
+++ devboard/tasks/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from tasks.models import Task
+from tasks.models import Task, Comment
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "project", "assigned_to", "status", "due_date")
     search_fields = ("title", "description")
     list_filter = ("status", "created_at")
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "task", "author", "content", "created_at")
+    search_fields = ("content",)
+    list_filter = ("created_at",)
